8695cd6e09dfc5dfabbc9ccb1986d934b61308db
Yusuf Adeniyi <easyclick05@gmail.com>
2021-04-30 12:51:01 +0100

    - azure app insight connected

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+//Collect Telemetry
+import * as appInsights from 'applicationinsights';
+
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -6,7 +9,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') || 3000;
+  appInsights.setup(configService.get('APPINSIGHTS_INSTRUMENTATIONKEY'));
   await app.listen(port);
+  appInsights.start();
   console.log(`app listening on port ${port}`);
 }
 bootstrap();
